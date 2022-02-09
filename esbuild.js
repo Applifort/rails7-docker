cda281850d46f31f40601436d20a40fcc3f7d421
Artur Gabitov <artur.gabitov@dualbootpartners.com>
2022-02-09 16:59:28 +0500

    fix esbuild

--- esbuild.js
+++ esbuild.js
@@ -1,20 +1,4 @@
-// function build(args) {
-//     require('esbuild')
-//       .build({
-//         entryPoints: ["app/javascript/application.js"],
-//         outdir: "app/assets/builds",
-//         bundle: true,
-//         plugins: [],
-//         watch: true,
-//         loader: {".js": "jsx"},
-//       })
-//       .catch(() => process.exit(1))
-//   }
-//   build(process.argv.slice(2))
-
-
-
-const { build } = require('esbuild')
+const { build } = require('esbuild');
 
 build({
     entryPoints: ["app/javascript/application.js"],
